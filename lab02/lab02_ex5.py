d533e345991ac94c16436b9ce3120be8a7e16848
Kalina Marinova <kalinka_marinova@hotmail.com>
2022-11-02 20:17:28 +0200

    lab02

--- lab02/lab02_ex5.py
+++ lab02/lab02_ex5.py
@@ -1,10 +1,38 @@
-print("Enter 2 or 16 to change 27102022 in one of those numerical systems.")
-a=27102022
-b=[]
-while a>0:
-    c=a%2
-    b.append(c)
-    a=a//2
+print("Enter 2 or 16 to convert 27102022 in one of those numerical systems.")
 
+num=27102022
+sys=int(input("Enter 2 or 16:"))
+a=[]
 
+if sys==2:
+    while (num>0):
+        b=num%2
+        a.append(b)
+        num=num//2
+    a.reverse()
+    for i in a:
+        print(i, end="")
+
+elif sys==16:
+    while (num>0):
+        b=num%16
+        if b==10:
+            b='A'
+        elif b==11:
+            b='B'
+        elif b==12:
+            b='C'
+        elif b==13:
+            b='D'
+        elif b==14:
+            b='E'
+        elif b==15:
+            b='F'
+        a.append(b)
+        num = num // 16
+    a.reverse()
+    for i in a:
+        print(i, end="")
+else:
+    print('invalid number input')
 
