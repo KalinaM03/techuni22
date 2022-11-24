1026ce0906dece290374cb021640049adf68291c
Kalina Marinova <kalinka_marinova@hotmail.com>
2022-11-24 10:19:49 +0200

    Homework from lab05

--- lab05/teams' chat tasks/ex2.py
+++ lab05/teams' chat tasks/ex2.py
@@ -1,13 +1,16 @@
-def list_avg(lst=[], multiplier=1):
+def list_avg(lst, multiplier=1):
+    num_list=[]
     if type(multiplier)!=int:
         print('invalid multiplier input')
     else:
         for i in lst:
-            # lst.append(int(i)*multiplier)
-            lst[lst.index(i)]=int(i)*multiplier
-    return lst
+            if type(i)==int or type(i)==float:
+                num_list.append(i*multiplier)
 
-print(list_avg([2,4,1],2))
+    print(f'List average is {round((sum(num_list)/len(num_list)),2)}')
+    return num_list
+
+print(list_avg([2,4,'1','a',4],2))
 
 
 
